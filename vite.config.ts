0f0f6d5a98b23f171cb58008c5085c04e37bed0c
Md Saimun Islam <71975044+saimundev@users.noreply.github.com>
2025-05-13 22:37:57 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Required for Docker to allow external access
+    host: '0.0.0.0', // Required for Docker to allow external access
     port: 5173, // Default Vite dev server port
   },
 });
